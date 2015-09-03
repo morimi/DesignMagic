3957494c6a7da01702d59b748197138cc2c1b514
mori_misaki <mori_misaki@cyberagent.co.jp>
2015-09-03 20:13:16 +0900

    エラーメッセージの持ち方を変更

--- common/jsx/checkLayerName.jsx
+++ common/jsx/checkLayerName.jsx
@@ -1,5 +1,48 @@
+/**
+ * @fileoverview layerSets & artLayersの命名チェック
+ */
 
-var mes = [];
+
+/**
+ * Result
+ * バリデーション結果
+ * @param {string} title
+ * @param {string} hint
+ * @param {string} type
+ */
+function Result(title, hint, type) {
+  this.data = {
+      title : title || null,
+      hint : hint || null,
+      type : type || null
+  };
+}
+
+/**
+ * @return {string} this.dataをJSON.stringifyした文字列
+ */
+Result.prototype.toString = function resultToString() {
+  //return JSON.stringify(this.data); JSON使えないよ
+  var d = this.data;
+  return '{title:"' + d.title + '", hint:"' + d.hint + '", type:"' + d.type + '"}';
+};
+
+
+/**
+ * メッセージ
+ */
+var VALIDATION_MESSAGE = {
+  NONAME : "命名されていません"
+};
+
+/**
+ * タイプ
+ */
+var VALIDATION_TYPE = {
+  WARN: "warn"
+};
+
+var mes = []; //エラーメッセージ文字列格納用
 var artLayers = []; //レイヤー配列をextendするための入れ物
 
 
@@ -12,20 +55,30 @@ function checkName(target) {
       var name = target[i].name;
       if (name.match(/レイヤー \d+/)) {
 
-        mes.push('{title:"' + name + '"'
-                + (target[i].parent && target[i].parent.typename == 'LayerSet' ?', parent: "' + target[i].parent.name + '"' : '')
-                 + ', hint:"命名されていません", type:"warn"}');
+       var result = new Result(name, VALIDATION_MESSAGE.NONAME, VALIDATION_TYPE.WARN);
+        mes.push(result.toString());
+
+//        mes.push('{title:"' + name + '"'
+//                + (target[i].parent && target[i].parent.typename == 'LayerSet' ?', parent: "' + target[i].parent.name + '"' : '')
+//                 + ', hint:"命名されていません", type:"warn"}');
       }
   }
 }
 
+/**
+ * LayerSetsの命名チェック
+ * @param {Array.<Object>} layerSets
+ * @return {Array.<Object>} メッセージの配列
+ */
 function checkSetName(target) {
   var l = target.length;
 
   for (var i = 0; l; i++) {
     var name = target[i].name;
     if ( name.match(/グループ \d+/) ) {
-      mes.push('{title:"' + name + '", hint:"命名されていません", type:"warn"}');
+     var result = new Result(name, VALIDATION_MESSAGE.NONAME, VALIDATION_TYPE.WARN);
+      mes.push(result.toString());
+      //mes.push('{title:"' + name + '", hint:"命名されていません", type:"warn"}');
     }
 
     if (target[i].artLayers.length ){
