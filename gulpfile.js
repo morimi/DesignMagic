633f91255a3405471ab95602cb169c448b357a03
kubo_hirotaka@cyberagent.co.jp <kubo_hirotaka@cyberagent.co.jp>
2015-11-27 16:19:53 +0900

    ディレクトリ構成変更。それにともなった package.json / gulp の変更。

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var exec = require('gulp-exec');
+var del  = require('del');
 
 
 // options
 var options = {
   path: {
+    src   : './src',
     build : './build',
     zxp   : './build/DesignMagic.zxp',
     cmd   : './tools/build/ZXPSignCmd',
@@ -28,9 +30,34 @@ var reportOptions = {
  * @param  {String} 'cert'
  */
 gulp.task( 'cert', function () {
-  gulp.src( '.' )
+  return gulp.src( '.' )
     .pipe( exec('<%= options.path.cmd %> -selfSignedCert JP Tokyo Cyberagent MisakiMori <%= options.passsword.cert %> <%= options.path.cert %>', options) )
-    .pipe( exec.reporter(reportOptions) )
+    .pipe( exec.reporter(reportOptions) );
+});
+
+/**
+ * src 内の node_modules を削除
+ * @param  {String} 'src-ndm-clean'
+ */
+gulp.task( 'src-ndm-clean', function (callback) {
+  del(options.path.src + '/node_modules', {force: false}, function (error, paths) {
+    if (error) {
+      console.log('error: ' + error);
+    } else {
+      console.log('clean: ' + paths);
+    }
+    callback();
+  });
+});
+
+/**
+ * node_modules を src 内にコピー
+ * @param  {String} 'src-ndm-copy'
+ */
+gulp.task( 'src-ndm-copy', function () {
+  return gulp.src( '.' )
+    .pipe( exec('cp -r ./node_modules <%= options.path.src %>/node_modules', options) )
+    .pipe( exec.reporter(reportOptions) );
 });
 
 
@@ -38,10 +65,15 @@ gulp.task( 'cert', function () {
  * build の後始末
  * @param  {String} 'clean'
  */
-gulp.task( 'clean', function () {
-  gulp.src( '.' )
-    .pipe( exec('rm -rf <%= options.path.build %>', options) )
-    .pipe( exec.reporter(reportOptions) )
+gulp.task( 'build-clean', function (callback) {
+  del(options.path.build, {force: false}, function (error, paths) {
+    if (error) {
+      console.log('error: ' + error);
+    } else {
+      console.log('clean: ' + paths);
+    }
+    callback();
+  });
 });
 
 
@@ -50,9 +82,9 @@ gulp.task( 'clean', function () {
  * @param  {String} 'build'
  */
 gulp.task( 'build', function () {
-  gulp.src( '.' )
+  return gulp.src( '.' )
     .pipe( exec('mkdir <%= options.path.build %>', options) )
-    .pipe( exec('<%= options.path.cmd %> -sign "." <%= options.path.zxp %> <%= options.path.cert %> <%= options.passsword.cert %>', options) )
+    .pipe( exec('<%= options.path.cmd %> -sign "<%= options.path.src %>" <%= options.path.zxp %> <%= options.path.cert %> <%= options.passsword.cert %>', options) )
     .pipe( exec.reporter(reportOptions) );
 });
 
@@ -62,4 +94,6 @@ gulp.task( 'build', function () {
  * @param  {String} 'default'
  * @param  {Array}
  */
-gulp.task( 'default', ['clean', 'build'] );
+gulp.task( 'build', ['build-clean', 'build'] );
+gulp.task( 'clean', ['build-clean', 'src-ndm-clean'] );
+gulp.task( 'ndm', ['src-ndm-clean', 'src-ndm-copy'] );
